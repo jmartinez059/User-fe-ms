0947778940b152ab5bcbde0cb00758327a09b9c1
Jason Martinez <jmartinez059@yahoo.com>
2019-09-25 23:28:36 -0500

    initial commit

---
+++ src/main/java/com/fifth/pillar/userfems/UserFeMsApplication.java
@@ -0,0 +1,13 @@
+package com.fifth.pillar.userfems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFeMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFeMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fifth/pillar/userfems/UserFeMsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fifth.pillar.userfems;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserFeMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
